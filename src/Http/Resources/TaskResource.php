70e7f55fb6ae5d0aa7ad073cf78d0bec7875452a
Siamak Samie <siamak.samie@gmail.com>
2023-06-29 12:40:25 -0400

    fixing task resource logic for task completed/expired

--- src/Http/Resources/TaskResource.php
+++ src/Http/Resources/TaskResource.php
@@ -12,37 +12,43 @@ class TaskResource extends JsonResource
 
     public function toArray($request)
     {
-
+        $time = $this->is_recurring ? Carbon::parse($this->time)->format('H:i') : $this->time;
         $completedTasks = $this->whenLoaded(TASK::JOB_SITE_SHIFTS_RELATION_NAME);
         $isMissingValue = $completedTasks instanceof MissingValue;
-        $isCompleted = false;
+
         if (!$isMissingValue) {
-            $isCompleted = $completedTasks->contains(function ($task) {
-                if (!$this->is_recurring) {
-                    return true;
-                }
-                $taskCompletedAt = $task['pivot']['created_at'];
-                $taskCompletedAt->setTimezone('America/Montreal');
-                $taskCompletedAt = $taskCompletedAt->toDateTimeString();
-                $shiftStart = carbon::parse($task['shift_start'])->subHours(4)->toDateTimeString();
-                $shiftEnd = carbon::parse($task['shift_end'])->addHours(4)->toDateTimeString();
-                return $taskCompletedAt >= $shiftStart && $taskCompletedAt <= $shiftEnd;
+            if ($this->is_recurring) {
+                $lastTaskToBeCompletedBy = Carbon::parse($this->shiftDay . ' ' . $time);
+            }
+            else{
+                $lastTaskToBeCompletedBy = $this->time;
+            }
+            $isExpired = Carbon::now()->diffInHours($lastTaskToBeCompletedBy) >= 8;
+            $isCompleted = $completedTasks->contains(function ($task) use ($lastTaskToBeCompletedBy, $time) {
+                $taskCompletedAt = $task['pivot']['created_at']->setTimezone('America/Montreal');
+                return $taskCompletedAt->diffInHours($lastTaskToBeCompletedBy) < 8;
             });
         }
 
-        return [
+        $resourceArray = [
             'id' => $this->id,
             'description' => $this->description,
             'contractId' => $this->contract_id,
             'jobSiteAddressId' => $this->job_site_address_id,
-            'time' => $this->is_recurring ? Carbon::parse($this->time)->format('H:i') : $this->time,
+            'time' => $time,
             'isRecurring' => $this->is_recurring,
             'employeeId' => $this->employee_id,
-            'isCompleted' => $isCompleted,
             'creator' => $this->whenLoaded('employee'),
             'jobSite' => new JobSiteResource($this->whenLoaded('jobSite')),
             'contract' => new ContractResource($this->whenLoaded('contract')),
-            'taskRecurrence' => TaskRecurrenceResource::collection($this->whenLoaded('taskRecurrence')),
+            'taskRecurrence' => TaskRecurrenceResource::collection($this->whenLoaded('taskRecurrence'))
         ];
+
+        if (!$isMissingValue) {
+            $resourceArray['isCompleted'] = $isCompleted;
+            $resourceArray['isExpired'] = $isExpired;
+        }
+
+        return $resourceArray;
     }
 }
